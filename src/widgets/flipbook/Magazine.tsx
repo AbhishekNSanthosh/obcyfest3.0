8884e4e9938d49ff4b4f497a8aca55496cb0bb7b
Joeljoy1237 <joeljoy1237@gmail.com>
2025-10-19 15:03:29 +0530

    fix : page number

--- src/widgets/flipbook/Magazine.tsx
+++ src/widgets/flipbook/Magazine.tsx
@@ -78,9 +78,14 @@ const PageCounter = React.memo(
     currentPage: number;
     totalPages: number;
   }) => (
-    <div className="mt-3 text-center text-sm font-medium text-yellow-500">
-      Page {currentPage} of {totalPages - 1}
+    <>
+    <div className="mt-3 text-center text-sm font-medium text-yellow-500 sm:hidden">
+      Page {currentPage-1} of {totalPages}
     </div>
+    <div className="mt-3 text-center text-sm font-medium text-yellow-500 hidden sm:block">
+      Page {currentPage} of {totalPages}
+    </div>
+    </>
   )
 );
 
